2446cb15b215c80b41ba11d3bd71ecbe4283069c
Kha Phạm Thành <kha@devpanel.com>
2023-06-19 17:49:45 +0700

    Strapi v4.10.7

---
+++ .devpanel/re-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ---------------------------------------------------------------------
+# Copyright (C) 2021 DevPanel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation version 3 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# For GNU Affero General Public License see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+cd $APP_ROOT
+npm install
